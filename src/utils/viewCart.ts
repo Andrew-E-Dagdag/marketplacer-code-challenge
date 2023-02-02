94fa8a168ada1fede2d55338375d79fead07f288
Andrew Dagdag <andrew.e.dagdag@gmail.com>
2023-02-02 21:52:27 +0800

    Added case of checking empty cart

--- src/utils/viewCart.ts
+++ src/utils/viewCart.ts
@@ -4,6 +4,13 @@ import { ShoppingCart } from "./types";
 
 export const viewCart = (Cart: ShoppingCart) => {
   const uuids = Object.keys(Cart);
+  if (uuids.length === 0) {
+    console.log(
+      'It looks like your cart is empty, add products by using the "Add [product index] [quantity]" command!'
+    );
+    return;
+  }
+
   const productDetails = getProductsByUUID(uuids);
   let cartTotal = 0;
   uuids.forEach((uuid) => {

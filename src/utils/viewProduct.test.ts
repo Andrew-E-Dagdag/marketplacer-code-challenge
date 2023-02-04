255e4ff95a1d59ca00292c186461b2ad69756498
Andrew Dagdag <andrew.e.dagdag@gmail.com>
2023-02-04 23:49:41 +0800

    Added tests for viewProduct

---
+++ src/utils/viewProduct.test.ts
@@ -0,0 +1,60 @@
+// export const viewProduct = (index: number) => {
+//   const product: Product = getProductByIndex(index);
+//   printProduct(product, index);
+//   console.log(
+//     `Description: ${product.description ?? "Lorem Ipsum Very Cool Product"}`
+//   );
+// };
+
+import { viewProduct } from "./viewProduct";
+import * as getProductByIndexModule from "./getProductByIndex";
+import { Product } from "./types";
+
+describe("View Product", () => {
+  jest.spyOn(getProductByIndexModule, "getProductByIndex");
+  const baseProduct: Product = {
+    name: "Very Cool Product",
+    uuid: 10101,
+    price: "5000.5"
+  };
+
+  beforeEach(() => {
+    jest.spyOn(console, "log");
+  });
+
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
+  it("Shows the product with its own description", () => {
+    const productDescription = "Very cool description here";
+    getProductByIndexModule.getProductByIndex = jest.fn().mockReturnValue({
+      ...baseProduct,
+      description: productDescription
+    });
+
+    viewProduct(1);
+
+    expect(console.log).toHaveBeenCalledWith(
+      `1. ${baseProduct.name} - $${baseProduct.price}`
+    );
+    expect(console.log).toHaveBeenCalledWith(
+      `Description: ${productDescription}`
+    );
+  });
+
+  it("Shows the product with the fallback description", () => {
+    getProductByIndexModule.getProductByIndex = jest
+      .fn()
+      .mockReturnValue(baseProduct);
+
+    viewProduct(1);
+
+    expect(console.log).toHaveBeenCalledWith(
+      `1. ${baseProduct.name} - $${baseProduct.price}`
+    );
+    expect(console.log).toHaveBeenCalledWith(
+      "Description: Lorem Ipsum Very Cool Product"
+    );
+  });
+});
